50099c1485442bec42e2df12d68ee55e6ccd5b87
shekharrajak <shekharrajak@live.com>
2019-06-28 13:48:17 +0530

    readme and custom theme configuration/installation added

---
+++ README.md
@@ -0,0 +1,21 @@
+- To Install the custom theme:
+
+```
+cd docs/custom_theme
+
+python setup.py install
+
+```
+
+- Once the custom theme is installed, use it in created docs :
+
+```
+
+cd docs
+
+make clean
+
+make html
+
+```
+
---
+++ docs/custom_theme/build/lib/spr_theme/__init__.py
@@ -0,0 +1,16 @@
+
+from os import path
+
+__version__ = '0.0.1'
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+    print(cur_dir)
+    return cur_dir
+
+def setup(app):
+    print(path.abspath(path.dirname(__file__)))
+    app.add_html_theme('custom_sphinx_theme', path.abspath(path.dirname(__file__)))
