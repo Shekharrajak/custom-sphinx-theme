7ed0589b7af552133110f9156f9419364b333cab
shekharrajak <shekharstudy@ymail.com>
2019-07-09 18:34:20 +0530

    minor change in sh file

--- generate_html.sh
+++ generate_html.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # echo $TRAVIS_COMMIT
 pin install numpy
-cd docs && GITVER=31465473c4 make html
+cd docs/src/custom-sphinx-theme && python setup.py install
+cd ../../docs && GITVER=31465473c4 make html
