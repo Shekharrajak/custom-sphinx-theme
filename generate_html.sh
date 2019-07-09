5f2e30d9fdfad0f26f328a2d1a314829c288eb23
shekharrajak <shekharstudy@ymail.com>
2019-07-09 18:11:30 +0530

    minor change in sh file

--- generate_html.sh
+++ generate_html.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-export NUMPYVER:=$(shell python3 -c "import numpy; print(numpy.version.git_revision[:10])")
-
-echo $NUMPYVER
-
-cd docs && GITVER=$(NUMPYVER) make html
+echo $TRAVIS_COMMIT
+cd docs && GITVER=$TRAVIS_COMMIT make html
